1ef38cb21aead315b7a19c6c113295b696ebfedb
nailahsalsabila27 <nailahslsb@gmail.com>
2024-09-11 16:40:02 +0700

    biodata nailah salsabila

---
+++ Biodata19.java
@@ -0,0 +1,14 @@
+public class Biodata19 {
+    public static void main(String[] args) {
+        System.out.println("Nama Lengkap    : Nailah Salsabila Maulidiyah");
+        System.out.println("NIM/NPM         : 244107020120");
+        System.out.println("Jenis Kelamin   : Perempuan");
+        System.out.println("Hobby           : Menonton Film");
+        System.out.println("Jurusan         : Teknologi Informasi");
+        System.out.println("Prodi           : D-IV Teknik Informatika");
+        System.out.println("Universitas     : Politeknik Negeri Malang");
+        System.out.println("TTL             : Pasurun, 27 April 2006");
+        System.out.println("Alamat          : Perum Pondok Mentari Blok G2/12A");
+    }
+    
+}
