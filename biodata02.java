df0b6d9273874d40bff6064c929178128dcd0cb2
arafas27 <kiritodd10@gmail.com>
2024-09-11 09:09:35 +0700

    biodata 02

--- biodata02.java
+++ biodata02.java
@@ -2,6 +2,7 @@ public class biodata02 {
     public static void main(String[] args) {
         System.out.println("Nama Lengkap     : Ahmad Putra Fajar Shdoiq");
         System.out.println("NIM/NPM          : 244107020130");
+        System.out.println("Jenis Kelamin    : Laki-laki");
         System.out.println("Jurusan          : Teknologi Informasi");
         System.out.println("Prodi            : D-IV Teknik INformatika");
         System.out.println("Universitas      : Politeknik Negeri Malang");
